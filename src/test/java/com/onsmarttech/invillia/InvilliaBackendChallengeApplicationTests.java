67dc178d0fff83a516e016572d0596537871641b
Elias Braz <elias.brazjunior@gmail.com>
2019-03-03 16:48:43 -0300

    Improving the tests

--- src/test/java/com/onsmarttech/invillia/InvilliaBackendChallengeApplicationTests.java
+++ src/test/java/com/onsmarttech/invillia/InvilliaBackendChallengeApplicationTests.java
@@ -1,6 +1,6 @@
 package com.onsmarttech.invillia;
 
-import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,9 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,17 +26,12 @@ public class InvilliaBackendChallengeApplicationTests {
 	protected TestRestTemplate template;
 
 	@Test
-	public void contextLoads() {
-	}
-
-	@Test
 	public void testUnauthorized() throws Exception {
-		mvc.perform(get("/stores").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+		mvc.perform(get("/stores/filter")).andExpect(status().isUnauthorized());
 	}
 
 	@Test
 	public void testAuthorized() throws Exception {
-		ResponseEntity<String> result = template.withBasicAuth("spring", "secret").getForEntity("/stores", String.class);
-		assertEquals(HttpStatus.OK, result.getStatusCode());
+		mvc.perform(get("/stores/filter").with(httpBasic("spring", "secret"))).andExpect(status().isOk());
 	}
 }
