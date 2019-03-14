3ad8d86da575619bdbfdc187bed744519e79f993
Elias Braz <elias.brazjunior@gmail.com>
2019-03-13 22:17:05 -0300

    Testing orders

--- src/test/java/com/onsmarttech/invillia/OrderTests.java
+++ src/test/java/com/onsmarttech/invillia/OrderTests.java
@@ -2,6 +2,7 @@ package com.onsmarttech.invillia;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ import java.util.Arrays;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onsmarttech.invillia.entities.Order;
 import com.onsmarttech.invillia.entities.OrderItem;
 import com.onsmarttech.invillia.entities.Store;
@@ -22,6 +24,15 @@ public class OrderTests extends InvilliaBackendChallengeApplicationTests {
 
 	@Test
 	public void testCreateOrderWithItems() throws Exception {
+		Store store = new Store();
+		store.setName("Peugeot");
+		store.setAddress("Centro - Rio de Janeiro - RJ");
+
+		String json = new ObjectMapper().writeValueAsString(store);
+
+		this.mvc.perform(post("/stores").with(httpBasic("spring", "secret")).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON_VALUE).content(json))
+				.andExpect(status().isCreated());
+
 		Order order = new Order();
 		order.setAddress("Rio de Janeiro - RJ");
 		order.setConfirmationDate(LocalDate.now());
@@ -36,6 +47,6 @@ public class OrderTests extends InvilliaBackendChallengeApplicationTests {
 		order.setOrderItems(Arrays.asList(item));
 
 		this.mvc.perform(post("/orders").with(httpBasic("spring", "secret")).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON_VALUE)
-				.content(JsonUtil.convertObjectToString(order))).andExpect(status().isCreated());
+				.content(JsonUtil.convertObjectToString(order))).andExpect(status().isCreated()).andDo(print()).andReturn();
 	}
 }
