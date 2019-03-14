7847c4bdcdceac86585ddbbac49b612574c10f7d
Elias Braz <elias.brazjunior@gmail.com>
2019-03-13 22:16:21 -0300

    Formatting dates

--- src/test/java/com/onsmarttech/invillia/utils/JsonUtil.java
+++ src/test/java/com/onsmarttech/invillia/utils/JsonUtil.java
@@ -2,11 +2,14 @@ package com.onsmarttech.invillia.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtil {
 
 	public static String convertObjectToString(Object object) throws JsonProcessingException {
-		return new ObjectMapper().writeValueAsString(object);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		return mapper.writeValueAsString(object);
 	}
 
 }
